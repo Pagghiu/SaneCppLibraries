8c9d66f4db7b5c473155f8d45fadf3b96966071f
Stefano Cristiano <pagghiu@gmail.com>
2023-01-05 01:00:01 +0100

    Tests: Improve SCTest naming

--- Libraries/Foundation/Memory.cpp
+++ Libraries/Foundation/Memory.cpp
@@ -23,6 +23,7 @@ void*           operator new[](SC::size_t len) { return malloc(len); }
 void*           __cxa_pure_virtual = 0;
 extern "C" int  __cxa_guard_acquire(SC::uint64_t* guard_object) { return 0; }
 extern "C" void __cxa_guard_release(SC::uint64_t* guard_object) {}
+extern "C" void __cxa_guard_abort(SC::uint64_t* guard_object) {}
 #endif
 
 void operator delete(void* p) noexcept
