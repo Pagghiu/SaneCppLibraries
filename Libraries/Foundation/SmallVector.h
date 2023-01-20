03046975aa7c9fbf2be60d4d7fea805415890148
Stefano Cristiano <pagghiu@gmail.com>
2023-01-20 17:47:57 +0100

    Foundation: Move SmallVector into its proper file

--- Libraries/Foundation/SmallVector.h
+++ Libraries/Foundation/SmallVector.h
@@ -2,6 +2,7 @@
 //
 // All Rights Reserved. Reproduction is not allowed.
 #pragma once
+#include "Array.h"
 #include "Vector.h"
 
 namespace SC
@@ -9,3 +10,40 @@ namespace SC
 template <typename T, int N>
 struct SmallVector;
 } // namespace SC
+
+template <typename T, int N>
+struct SC::SmallVector : public Vector<T>
+{
+    Array<T, N> buffer;
+    SmallVector()
+    {
+        SegmentHeader* header         = SegmentHeader::getSegmentHeader(buffer.items);
+        header->options.isSmallVector = true;
+        Vector<T>::items              = buffer.items;
+    }
+    SmallVector(SmallVector&& other) : Vector<T>(forward<Vector<T>>(other)) {}
+    SmallVector(const SmallVector& other) : Vector<T>(other) {}
+    SmallVector& operator=(SmallVector&& other)
+    {
+        Vector<T>::operator=(forward<Vector<T>>(other));
+        return *this;
+    }
+    SmallVector& operator=(const SmallVector& other)
+    {
+        Vector<T>::operator=(other);
+        return *this;
+    }
+
+    SmallVector(Vector<T>&& other) : Vector<T>(forward<Vector<T>>(other)) {}
+    SmallVector(const Vector<T>& other) : Vector<T>(other) {}
+    SmallVector& operator=(Vector<T>&& other)
+    {
+        Vector<T>::operator=(forward<Vector<T>>(other));
+        return *this;
+    }
+    SmallVector& operator=(const Vector<T>& other)
+    {
+        Vector<T>::operator=(other);
+        return *this;
+    }
+};
--- Libraries/Foundation/SmallVectorTest.h
+++ Libraries/Foundation/SmallVectorTest.h
@@ -9,42 +9,6 @@ namespace SC
 {
 struct SmallVectorTest;
 }
-template <typename T, int N>
-struct SC::SmallVector : public Vector<T>
-{
-    Array<T, N> buffer;
-    SmallVector()
-    {
-        SegmentHeader* header         = SegmentHeader::getSegmentHeader(buffer.items);
-        header->options.isSmallVector = true;
-        Vector<T>::items              = buffer.items;
-    }
-    SmallVector(SmallVector&& other) : Vector<T>(forward<Vector<T>>(other)) {}
-    SmallVector(const SmallVector& other) : Vector<T>(other) {}
-    SmallVector& operator=(SmallVector&& other)
-    {
-        Vector<T>::operator=(forward<Vector<T>>(other));
-        return *this;
-    }
-    SmallVector& operator=(const SmallVector& other)
-    {
-        Vector<T>::operator=(other);
-        return *this;
-    }
-
-    SmallVector(Vector<T>&& other) : Vector<T>(forward<Vector<T>>(other)) {}
-    SmallVector(const Vector<T>& other) : Vector<T>(other) {}
-    SmallVector& operator=(Vector<T>&& other)
-    {
-        Vector<T>::operator=(forward<Vector<T>>(other));
-        return *this;
-    }
-    SmallVector& operator=(const Vector<T>& other)
-    {
-        Vector<T>::operator=(other);
-        return *this;
-    }
-};
 
 struct SC::SmallVectorTest : public SC::TestCase
 {
