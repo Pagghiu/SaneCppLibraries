421041bfd7c85b1bd1d9ad5ec93266379ed503a7
Stefano Cristiano <pagghiu@gmail.com>
2023-04-04 00:33:57 +0200

    Foundation: Add UniquePtr

---
+++ Libraries/Foundation/UniquePtr.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2022-2023, Stefano Cristiano
+//
+// All Rights Reserved. Reproduction is not allowed.
+#pragma once
+#include "Language.h"
+
+namespace SC
+{
+template <typename T>
+struct UniquePtr
+{
+    UniquePtr() = default;
+    ~UniquePtr() { delete ptr; }
+    UniquePtr(UniquePtr&& other) { swap(ptr, other.ptr); }
+    UniquePtr& operator=(UniquePtr&& other)
+    {
+        delete ptr;
+        ptr = nullptr;
+        swap(ptr, other.ptr);
+        return *this;
+    }
+    UniquePtr(const UniquePtr&)            = delete;
+    UniquePtr& operator=(const UniquePtr&) = delete;
+
+    T*       operator->() { return ptr; }
+    const T* operator->() const { return ptr; }
+    T&       operator*() { return *ptr; }
+    const T& operator*() const { return *ptr; }
+
+  private:
+    T* ptr = nullptr;
+    template <typename Q, typename... Args>
+    friend UniquePtr<Q> MakeUnique(Args&&... args);
+
+    UniquePtr(T* ptr) : ptr(ptr){};
+};
+
+template <typename T, typename... Args>
+UniquePtr<T> MakeUnique(Args&&... args)
+{
+    return UniquePtr<T>(new T(forward<Args>(args)...));
+}
+} // namespace SC
