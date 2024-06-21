1f332027902f80e829a5fa5ff690675982287600
Stefano Cristiano <pagghiu@gmail.com>
2024-06-21 23:50:25 +0200

    Reflection: Disable SC_REFLECT_AUTOMATIC on older clang versions

--- Tests/SCTest/SCConfig.h
+++ Tests/SCTest/SCConfig.h
@@ -7,5 +7,8 @@
 // - Declaring a constructor on a struct will break the member detection
 // Issue so far when in C++ 20:
 // - Cannot auto-deduce structs that inherit from other
-#define SC_REFLECT_AUTOMATIC           1
+#if !defined(__clang_major__) || (__clang_major__ > 10)
+#define SC_REFLECT_AUTOMATIC 1
+#endif
+
 #define SC_LANGUAGE_FORCE_STANDARD_CPP 14
