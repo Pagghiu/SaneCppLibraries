fcbe7837a0ceba30792fb94d9a5078f68983f307
Stefano Cristiano <pagghiu@gmail.com>
2025-10-16 22:13:40 +0200

    Foundation: Fully qualify SC::forward to avoid confusion with std::forward

--- Libraries/Foundation/PrimitiveTypes.h
+++ Libraries/Foundation/PrimitiveTypes.h
@@ -90,7 +90,7 @@ inline void* operator new[](SC::size_t, void* p, SC::PlacementNew) noexcept { re
 namespace SC
 {
 /// Placement New
-template<typename T, typename... Q> void placementNew(T& storage, Q&&... other) { new (&storage, PlacementNew()) T(forward<Q>(other)...); }
+template<typename T, typename... Q> void placementNew(T& storage, Q&&... other) { new (&storage, PlacementNew()) T(SC::forward<Q>(other)...); }
 template<typename T> void placementNewArray(T* storage, size_t size) { new (storage, PlacementNew()) T[size]; }
 template<typename T> void dtor(T& t){ t.~T(); }
 }
