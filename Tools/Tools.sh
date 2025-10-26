f646f509d272b7a8a66695c882e5dc755060780a
Stefano Cristiano <pagghiu@gmail.com>
2025-10-26 09:18:54 +0100

    Tools: Print how much time is being spent compiling a Tool

--- Tools/Tools.sh
+++ Tools/Tools.sh
@@ -42,17 +42,31 @@ execute_tool () {
 
 OS=$(uname -s)
 
+# Time the initial build
+start_time=$(date +%s%N)
 call_make build
+initial_exit_code=$?
+end_time=$(date +%s%N)
+build_time=$(( (end_time - start_time) / 1000000000 ))
+build_time_frac=$(( (end_time - start_time) % 1000000000 / 1000000 ))
+printf "Time to compile \"${TOOL_NAME}\" tool: %d.%03d seconds\n" $build_time $build_time_frac
 
-if [ $? -eq 0 ]; then
+if [ $initial_exit_code -eq 0 ]; then
     execute_tool "$@"
 else
     # It could have failed because of moved files, let's re-try after cleaning
     call_make clean
+    # Time the clean and rebuild
+    start_time=$(date +%s%N)
     call_make build
-    if [ $? -eq 0 ]; then
+    rebuild_exit_code=$?
+    end_time=$(date +%s%N)
+    rebuild_time=$(( (end_time - start_time) / 1000000000 ))
+    rebuild_time_frac=$(( (end_time - start_time) % 1000000000 / 1000000 ))
+    printf "Time to re-compile \"${TOOL_NAME}\" tool after clean: %d.%03d seconds\n" $rebuild_time $rebuild_time_frac
+    if [ $rebuild_exit_code -eq 0 ]; then
         execute_tool "$@"
     else
-        exit $?
+        exit $rebuild_exit_code
     fi
 fi
