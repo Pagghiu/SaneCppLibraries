714ecf7c4729f4efe590c152c4636b84ad536adf
Stefano Cristiano <pagghiu@gmail.com>
2026-06-07 00:22:16 +0200

    Http: Add public header compile smoke
    Compile the exported SaneCppHttp.h umbrella in SCTest and touch representative parser, header, routing, multipart, async, and WebSocket API symbols so public include regressions fail early.

---
+++ Tests/Tools/SCHttpPublicHeaderCompileTest.cpp
@@ -0,0 +1,87 @@
+// Copyright (c) Stefano Cristiano
+// SPDX-License-Identifier: MIT
+#include "SaneCppHttp.h"
+
+namespace
+{
+auto* percentDecodePointer          = &SC::HttpPercentDecode;
+auto* formUrlDecodePointer          = &SC::HttpFormUrlDecode;
+auto* jsonContentTypePointer        = &SC::HttpContentTypeApplicationJson;
+auto* parseBearerTokenPointer       = &SC::HttpParseBearerToken;
+auto* writeBearerAuthPointer        = &SC::HttpWriteBearerAuthorization;
+auto* websocketCreateKeyPointer     = &SC::HttpWebSocketHandshake::createClientKey;
+auto* websocketComputeAcceptPointer = &SC::HttpWebSocketHandshake::computeAccept;
+
+struct ConsumePublicHttpHeaderSymbols
+{
+    ConsumePublicHttpHeaderSymbols()
+    {
+        SC::HttpParser                          parser;
+        SC::HttpURLParser                       url;
+        SC::HttpRequestTargetView               target;
+        SC::HttpURLQueryItem                    queryItem;
+        SC::HttpURLQueryIterator                queryIterator(SC::StringSpan{});
+        SC::HttpFormUrlEncodedIterator          formIterator(SC::StringSpan{});
+        SC::HttpCookieIterator                  cookieIterator(SC::StringSpan{});
+        SC::HttpSetCookieView                   cookie;
+        SC::HttpSetCookieBuilder                cookieBuilder;
+        SC::HttpCacheControlBuilder             cacheControl;
+        SC::HttpAuthorizationView               authorization;
+        SC::HttpRoute                           route;
+        SC::HttpRouteMatch                      routeMatch;
+        SC::HttpRouter                          router;
+        SC::HttpAsyncServerTlsOptions           serverTls;
+        SC::HttpAsyncClientTlsOptions           clientTls;
+        SC::HttpAsyncClient::Header             clientHeader;
+        SC::HttpAsyncClient::RequestOptions     request;
+        SC::HttpAsyncFileServerOptions          fileServerOptions;
+        SC::HttpMultipartContentDispositionView multipartDisposition;
+        SC::HttpMultipartPartHeadersView        multipartHeaders;
+        SC::HttpMultipartParser                 multipartParser;
+        SC::HttpWebSocketFrameHeaderView        frame;
+        SC::HttpWebSocketFrameReader            frameReader;
+        SC::HttpWebSocketFrameWriter            frameWriter;
+        SC::HttpWebSocketEndpoint               endpoint;
+        SC::HttpWebSocketConnectionPump         pump;
+        SC::HttpWebSocketSmallHub               hub;
+
+        parser.type    = SC::HttpParser::Type::Request;
+        route.method   = SC::HttpParser::Method::HttpGET;
+        frame.opcode   = SC::HttpWebSocketOpcode::Text;
+        request.method = SC::HttpParser::Method::HttpPOST;
+
+        frameReader.reset(SC::HttpWebSocketEndpointRole::Server);
+        frameWriter.reset(SC::HttpWebSocketEndpointRole::Client);
+        endpoint.reset(SC::HttpWebSocketEndpointRole::Server);
+
+        (void)percentDecodePointer;
+        (void)formUrlDecodePointer;
+        (void)jsonContentTypePointer;
+        (void)parseBearerTokenPointer;
+        (void)writeBearerAuthPointer;
+        (void)websocketCreateKeyPointer;
+        (void)websocketComputeAcceptPointer;
+        (void)url;
+        (void)target;
+        (void)queryItem;
+        (void)queryIterator;
+        (void)formIterator;
+        (void)cookieIterator;
+        (void)cookie;
+        (void)cookieBuilder;
+        (void)cacheControl;
+        (void)authorization;
+        (void)routeMatch;
+        (void)router;
+        (void)serverTls;
+        (void)clientTls;
+        (void)clientHeader;
+        (void)fileServerOptions;
+        (void)multipartDisposition;
+        (void)multipartHeaders;
+        (void)multipartParser;
+        (void)pump;
+        (void)hub;
+    }
+} consumePublicHttpHeaderSymbols;
+} // namespace
